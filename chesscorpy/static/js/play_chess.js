d3273ff20cae530c8658db11e3bbcbdca0499138
kurtjd <kurtisdinelle@gmail.com>
2021-08-16 15:10:49 -0700

    Implement restriction to legal moves and move highlighting

--- chesscorpy/static/js/play_chess.js
+++ chesscorpy/static/js/play_chess.js
@@ -1,6 +1,73 @@
-var config = {
-    position: 'start',
-    draggable: true,
+function unHighlightSquares()
+{
+    $('#board .square-55d63').css('background', '')
 }
 
-var board = Chessboard('board', config)
+function highlightSquare(square)
+{
+    var $square = $('#board .square-' + square)
+
+    var background = '#a9a9a9'
+    if ($square.hasClass('black-3c85d'))
+    {
+        background = '#696969'
+    }
+
+    $square.css('background', background)
+}
+
+function onDragStart(source, piece, position, orientation)
+{
+    if (game.turn() != piece[0])
+        return false
+}
+
+function onDrop(source, target)
+{
+    unHighlightSquares()
+
+    var move = game.move({
+        from: source,
+        to: target
+    })
+
+    if (move == null)
+        return "snapback"
+}
+
+function onMouseoverSquare(square, piece)
+{
+    var moves = game.moves({
+        square: square,
+        verbose: true
+    })
+
+    if (moves.length === 0)
+        return
+
+    highlightSquare(square)
+
+    for (var i = 0; i < moves.length; i++)
+    {
+        highlightSquare(moves[i].to)
+    }
+}
+
+function onMouseoutSquare(square, piece)
+{
+    unHighlightSquares()
+}
+
+
+var board_config = {
+    position: "start",
+    draggable: true,
+    onDragStart: onDragStart,
+    onDrop: onDrop,
+    onMouseoverSquare: onMouseoverSquare,
+    onMouseoutSquare: onMouseoutSquare
+}
+
+var game = new Chess()
+var board = Chessboard("board", board_config)
+
